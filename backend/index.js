3d43a19ec70585ea9b6aa2f55b24369867b4867f
sephson <disutj@gmail.com>
2021-07-25 20:38:07 +0100

    socket

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,16 @@ const userRoute = require("./Routes/UserRoute");
 const postRoute = require("./Routes/PostRoute");
 const chatRoute = require("./Routes/ChatRoute");
 const myConversationRoute = require("./Routes/MyConversationRoute");
-
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-
 const app = express();
-
 dotenv.config();
+
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 mongoose.connect(
   process.env.MONGO_URL,
@@ -31,11 +34,50 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/conversation", myConversationRoute);
+
+io.on("connection", (socket) => {
+  console.log("A User Connected");
+
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("someone disconnnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server running on port ${process.env.PORT}`);
