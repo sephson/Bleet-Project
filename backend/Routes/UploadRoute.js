99f1a7be86a9b50955a1658b45bb04283318f819
sephson <disutj@gmail.com>
2021-08-23 11:13:59 +0100

    oo

--- backend/Routes/UploadRoute.js
+++ backend/Routes/UploadRoute.js
@@ -1,17 +1,35 @@
-const express = require("express");
+const express = require("express")
 const router = express.Router();
 const path = require("path");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "uploads/");
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
   },
-  filename: (req, file, callback) => {
-    callback(null, `${file.originalname}`);
+  filename: (req, file, cb) => {
+    cb( null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
   },
 });
-const upload = multer({ storage });
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = filetypes.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    cb('Images only!')
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb)
+  },
+})
 
 router.put("/", upload.single("image"), (req, res) => {
   res.send(`/${req.file.path}`);
