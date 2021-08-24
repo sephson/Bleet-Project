ffed1fdda6e35f9b1026d8f7fd6bbbfbf2b3bec4
sephson <disutj@gmail.com>
2021-08-24 11:04:56 +0100

    adding url encoded

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ mongoose.connect(
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.json());
+// app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
